cbe8fec96ea9e940e59c0a55a31df84a9afd14ec
Erlend Røsjø <erlend.rosjo@spv.no>
2022-10-28 14:46:22 +0200

    WIP

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./backend/routes.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("frontend"));
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Application started on port ${port}`);
+});
