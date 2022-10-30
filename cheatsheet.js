b2ca8e5fd91098d7c3fde9ff4fde3fd63318e99b
Erlend Røsjø <erlend@erlend.net>
2022-10-30 15:47:25 +0100

    Update cheatsheet.js

--- cheatsheet.js
+++ cheatsheet.js
@@ -123,6 +123,22 @@ console.log(lastRegionName); 'Rogaland'
 
 let { regions: [ , ...{ counties: [, lastRegionLastCounty] }] } = norway2;
 
+// 
+// === Object/array spread ===
+// Spread is a neat way of copying and merging
+
+// Array spread
+const array = [1, 2, 3];
+const newArray = [-1, 0, ...array]; // [-1, 0, 1, 2, 3]
+
+const arr1 = [0, 1, 2];
+const arr2 = [3, 4, 5];
+const arr3 = [...arr1, ...arr2]; // [0, 1, 2, 3, 4, 5]
+
+// Object spread
+const person = { firstName: 'Ola', lastName: 'Nordmann' };
+const newPerson = { age: 20, ...person }; // { age:20, firstName: 'Ola', lastName: 'Nordmann' }
+
 //
 // === Functions ===
 // Functions are first-class citizens in JavaScript and is the encapsulation/scoping
