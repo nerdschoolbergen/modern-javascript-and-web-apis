77dd40a25dd8fd5df0f74fabf2d7dbf0654bb668
Erlend Røsjø <erlend.rosjo@spv.no>
2024-10-15 13:48:02 +0200

    Automatically refresh the browser on file changes

--- solution/app.js
+++ solution/app.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import http from 'http';
+import reload from 'reload';
 import routes from './backend/routes.js';
 
 const app = express();
 const port = 3000;
 
+app.set('port', process.env.PORT || port)
 app.use(express.static('frontend'));
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Application started on http://localhost:${port}`);
+
+var server = http.createServer(app);
+
+reload(app).then(() => {
+  server.listen(app.get('port'), function () {
+    console.log(`Application started on http://localhost:${app.get('port')}`);
+  })
+}).catch(function (err) {
+  console.error('Application could to be started:', err)
 });
